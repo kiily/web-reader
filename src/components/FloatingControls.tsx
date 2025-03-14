c2b00c0bdc0f8c4284640b7d6f1876877ac9f826
kiily <miguelmarin95@hotmail.com>
2025-03-14 21:06:42 +0000

    feat: smooth drag baby

--- src/components/FloatingControls.tsx
+++ src/components/FloatingControls.tsx
@@ -433,14 +433,10 @@ export default function FloatingControls({
 							: 'hover:shadow-md'
 					} transition-all duration-200 flex items-center space-x-2`}
 				>
-					{/* Speed Adjustment */}
-					<button
-						className="text-gray-500 dark:text-gray-400 p-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full"
-						onClick={(e) => {
-							e.stopPropagation();
-							onScrollDirectionToggle();
-						}}
-						aria-label="Adjust Scroll Speed"
+					{/* Drag Indicator */}
+					<div
+						className="text-gray-400 dark:text-gray-500 flex items-center justify-center"
+						aria-hidden="true"
 					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -448,13 +444,9 @@ export default function FloatingControls({
 							viewBox="0 0 20 20"
 							fill="currentColor"
 						>
-							<path
-								fillRule="evenodd"
-								d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-								clipRule="evenodd"
-							/>
+							<path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
 						</svg>
-					</button>
+					</div>
 
 					{/* Play/Pause Button */}
 					<button
